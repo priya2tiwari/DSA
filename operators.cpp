54080fa37e2659a967a3cbda45b91f83e0f0b831
priya2tiwari <100825783+priya2tiwari@users.noreply.github.com>
2023-02-15 20:02:24 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // bool a = true;
+    // bool b = false;
+    // cout<< (a|b);
+    //  cout<< (a&b);
+    //   cout<< (a^b);
+    //   cout<< (2&3);
+    //left shift
+    //   int a = 12;
+    //  int b = a<<2;
+    //   cout<<b<<endl;
+    //   int a =12;
+    //   a = a << -1;
+    //   cout<<a;//will trow garbage value
+      
+      //right shift
+    //   int a= 12;
+    //   a = a>>1;
+    //   cout<<a;
+
+    //pre and post increment
+    // int a = 11;
+    // //cout<< a <<endl;
+    // //cout<< ++a <<endl;
+    // cout<< a++ <<endl;
+    // cout<< a <<endl;
+    // int a = 6;
+    // int c = (a++)+2;
+    // cout<<c;
+
+    //variable scoping
+    //declaration
+    // int a;
+    // //initialisation
+    // int b = 5;
+    // //updation
+    // b=10;
+
+}
